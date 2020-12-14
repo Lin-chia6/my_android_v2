6935b2785f28a6a54c5c5838c5611952a92331a3
liu <lamigo9876@gmail.com>
2020-12-14 14:46:21 +0800

    已經實作了資料視覺化 並且成功的連上資料庫

---
+++ MyApplicationrecycle_view/app/src/main/java/com/example/myapplicationrecycle_view/basic.java
@@ -0,0 +1,88 @@
+package com.example.myapplicationrecycle_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class basic extends AppCompatActivity {
+
+    private Button return_to_uppage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic);
+
+        return_to_uppage = findViewById(R.id.button);
+        return_to_uppage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(basic.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+
+
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Rouge", 80540));
+        data.add(new ValueDataEntry("Foundation", 94190));
+        data.add(new ValueDataEntry("Mascara", 102610));
+        data.add(new ValueDataEntry("Lip gloss", 110430));
+        data.add(new ValueDataEntry("Lipstick", 128000));
+        data.add(new ValueDataEntry("Nail polish", 143760));
+        data.add(new ValueDataEntry("Eyebrow pencil", 170670));
+        data.add(new ValueDataEntry("Eyeliner", 213210));
+        data.add(new ValueDataEntry("Eyeshadows", 249980));
+
+        Column column = cartesian.column(data);
+
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("${%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+        cartesian.title("Top 10 Cosmetic Products by Revenue");
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title("Product");
+        cartesian.yAxis(0).title("Revenue");
+
+        anyChartView.setChart(cartesian);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.time_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
